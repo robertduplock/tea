3742b6dd6156eca88beb549c7440bf0cd2e50a64
robertduplock <robertduplock@gmail.com>
2016-09-02 09:33:41 +1000

    Added images for teas, JavaScript DOM improvements, notifications now work on mobile chrome FF

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,6 @@
-// Tea Object
+// Tea Object, Tea Time(r)
 var tea = {
+
     type: "Black",
     tempc: "95",
     tempf: "205",
@@ -7,6 +8,7 @@ var tea = {
     time: "04",
     url: "/teas",
     sound: true,
+
     getTeaInfo: function(teaType) {
         getAjax(this.url, function(text) {
             var data = JSON.parse(text);
@@ -15,10 +17,10 @@ var tea = {
                 if (item.type == teaType) {
                     tea.setTeaInfo(item);
                 }
-            });
-            
+            });            
         });
     },
+
     setTeaInfo: function(teaObj) {
         this.type = teaObj.type;
         this.tempc = teaObj.tempc;
@@ -26,38 +28,45 @@ var tea = {
         this.time = teaObj.time;
         this.setPage();
     },
+
     setPage: function() {
-        document.getElementById("tea-type-right").innerHTML = this.type;
-        document.getElementById("tea-time-right").innerHTML = this.time;
+        var teaSettings = document.getElementById("tea-settings");
+        teaSettings.querySelector("#tea-type-right").innerHTML = this.type;
+        teaSettings.querySelector("#tea-time-right").innerHTML = this.time.toString().slice(1);
         if (this.tempChoice == "c") {
-            document.getElementById("tea-temp-right").innerHTML = this.tempc + "&deg;C";
+            teaSettings.querySelector("#tea-temp-right").innerHTML = this.tempc + "&deg;C";
         } else {
-            document.getElementById("tea-temp-right").innerHTML = this.tempf + "&deg;F";
+            teaSettings.querySelector("#tea-temp-right").innerHTML = this.tempf + "&deg;F";
         }
-        document.getElementById("timer-set").innerHTML = "Timer Set:";
-        document.getElementById("timer").innerHTML = this.time + ":00";
+        teaSettings.querySelector("#timer-set").innerHTML = "Timer Set:";
+        teaSettings.querySelector("#timer").innerHTML = this.time + ":00";
         document.getElementById(this.type).className += " selected";
     },
+
     setTimer: function() {
         var duration = this.time * 60;
         this.startTimer(duration);
     },
-    toggleTemp: function() {        
-        var temp = document.getElementById("switch");
+
+    toggleTemp: function() {
+        var teaSettings = document.getElementById("tea-settings");
         if (this.tempChoice == "c") {
-            document.getElementById("tea-temp-right").innerHTML = this.tempf + "&deg;F";
-            temp.innerText = "Switch To Celsius";
+            teaSettings.querySelector("#tea-temp-right").innerHTML = this.tempf + "&deg;F";
+            teaSettings.querySelector("#switch").innerText = "Switch To Celsius";
             this.tempChoice = "f";
         } else {
-            document.getElementById("tea-temp-right").innerHTML = this.tempc + "&deg;C";
-            temp.innerText = "Switch To Fahrenheit";
+            teaSettings.querySelector("#tea-temp-right").innerHTML = this.tempc + "&deg;C";
+            teaSettings.querySelector("#switch").innerText = "Switch To Fahrenheit";
             this.tempChoice = "c";
         }
     },
+
     startTimer: function(duration) {
-        document.getElementById('steeper').setAttribute("disabled", "disabled");
+        notify.check(true);
+        var teaSettings = document.getElementById("tea-settings");
+        teaSettings.querySelector('#steeper').setAttribute("disabled", "disabled");
         var timer = duration, minutes, seconds;
-        var myTimer = document.getElementById("timer");
+        var myTimer = teaSettings.querySelector("#timer");
         var interval = setInterval(function () {
             minutes = parseInt(timer / 60, 10)
             seconds = parseInt(timer % 60, 10);
@@ -75,13 +84,96 @@ var tea = {
                     audio.play();
                 }
                 myTimer.textContent = "0:00";
-                document.getElementById("timer-set").innerHTML = "Time Is Up!";
-                document.getElementById("blackOut").style.display = "block"; 
-                document.getElementById("timer-done").style.display = "block";
+                teaSettings.querySelector("#timer-set").innerHTML = "Time Is Up!";
+                var popup = document.getElementById("popup-wrapper");
+                popup.querySelector("#blackOut").style.display = "block"; 
+                popup.querySelector("#timer-done").style.display = "block";
                 window.scrollTo(0, 0);
+                notify.spawn("Tea Timer(r)", "Tea Timer Is Up!", "images/tea-black.png", "Tea Timer Is Up!", 5000);
             }
         }.bind(this), 1000);
     }
+
+}
+
+// generic html5 browser notifications object
+
+var notify = {
+
+    supported: false,
+    permission: false,
+    isMobile: false,
+    debug: false,
+
+    check: function(debug) {      
+        debug ? this.debug = debug : this.debug = false;
+        if (!("Notification" in window)) {
+            this.supported = false;
+            this.debug ? console.log('Notifications: *not* supported with this browser') : null;
+        } else {
+            this.supported = true;
+            this.debug ? console.log('Notifications: supported with this browser') : null;
+        }
+        this.request();
+    },
+
+    request: function() {
+        if (this.supported) {
+            if (Notification.permission == "granted") {
+                this.permission = true;
+            } else {
+                Notification.requestPermission().then(function(result) {
+                    if (result == "granted") {
+                        this.permission = true;
+                    } else {
+                        this.permission = false;
+                    }
+                });
+            }
+            this.debug ? console.log('Permission: ' + Notification.permission) : null;
+        }
+    },
+
+    spawn: function(title, body, icon, tempTitle, timeOut) {
+            // console.log(1);
+        if (this.supported && this.permission) {
+            var options = {};
+            if (body) {
+                options.body = body;
+            }
+            if (icon) {
+                options.icon = icon;
+            }
+            // var n = new Notification(title, options);
+            navigator.serviceWorker.register('sw.js');
+            navigator.serviceWorker.ready.then(function(registration) {
+                registration.showNotification(title, options);
+            });
+            window.focus();
+            if (tempTitle) {
+                var oldTitle = document.title;
+                document.title = this.tempTitle;
+                setTimeout(function(){
+                    document.title = oldTitle;
+                }, timeOut);
+            }
+            if (timeOut) {
+                setTimeout(function(){
+                    n.close();
+                }, timeOut);
+            }
+        } else {
+            this.debug ? console.log("Notification not spawning: Browser Support: ", this.supported, " Permission: ", this.permission) : null;
+        }
+    },
+    
+    checkMobile: function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+        this.isMobile = check;
+        return check;
+    }
+
 }
 
 tea.setPage();
@@ -104,18 +196,19 @@ function setEventListeners() {
             });
         })();
     }
-
-    document.getElementById("steeper").addEventListener("click", function(){
+    
+    var teaSettings = document.getElementById("tea-settings");
+    teaSettings.querySelector("#steeper").addEventListener("click", function(){
         tea.setTimer();
     });
-
-    document.getElementById("switch").addEventListener("click", function(){
+    teaSettings.querySelector("#switch").addEventListener("click", function(){
         tea.toggleTemp();
     });
 
-   document.getElementById("timer-done-close").addEventListener("click", function(){
-        document.getElementById("blackOut").style.display = "none"; 
-        document.getElementById("timer-done").style.display = "none"; 
+    var popup = document.getElementById("popup-wrapper");
+    popup.querySelector("#timer-done-close").addEventListener("click", function(){
+        popup.querySelector("#blackOut").style.display = "none"; 
+        popup.querySelector("#timer-done").style.display = "none"; 
         document.getElementById('steeper').removeAttribute("disabled");
         tea.getTeaInfo(tea.type);
     });
@@ -124,7 +217,6 @@ function setEventListeners() {
         tea.sound == false ? tea.sound = true : tea.sound = false;
         var alarm = document.getElementById("mute");
         tea.sound == false ? alarm.className = "muted" : alarm.classList.remove("muted");
-        console.log(tea.sound);
    });
 
 }
@@ -141,11 +233,4 @@ function getAjax(url, callback) {
         }
     };
     xhr.send();
-}
-
-// Test if notifications enabled (for future use)
-if ('Notification' in window) {
-  Notification.requestPermission();
-} else {
-  console.log("API not supported");
 }
